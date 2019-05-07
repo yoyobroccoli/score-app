22359dfab0f0bf316615ec0055657fea55c1250a
yoyobroccoli <yutta@kindwall.me>
2019-05-06 20:16:42 -0500

    Updates

---
+++ precache-manifest.9ffa42c68a0a08ff7f06b0214c2a9ecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d798fe3ae19cd77a56074322bede4b",
+    "url": "/score-app/index.html"
+  },
+  {
+    "revision": "bc52345b5f3f762394c5",
+    "url": "/score-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/score-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "bc52345b5f3f762394c5",
+    "url": "/score-app/static/js/main.f7a78bad.chunk.js"
+  },
+  {
+    "revision": "72f512b6f3cebecb598d",
+    "url": "/score-app/static/js/runtime~main.2035bdec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/score-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
